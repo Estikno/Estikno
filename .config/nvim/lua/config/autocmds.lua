94a19e047120072286f7991ae0b3d943ce8d6c13
Estikno <95084542+Estikno@users.noreply.github.com>
2025-08-14 18:51:47 +0200

    Add fastfetch and modifiet little things

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,28 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Eliminates padding when running in an Alacritty terminal
+
+-- Only run if inside Alacritty
+if vim.env.ALACRITTY_WINDOW_ID then
+  local function set_padding(x, y)
+    vim.fn.jobstart({
+      "alacritty",
+      "msg",
+      "config",
+      "window.padding.x=" .. x,
+      "window.padding.y=" .. y,
+    }, { detach = true })
+  end
+
+  -- Remove padding on enter
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      set_padding(0, 0)
+    end,
+  })
+
+  -- Restore padding on exit
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    callback = function()
+      set_padding(5, 5) -- <- change to your default padding
+    end,
+  })
+end
