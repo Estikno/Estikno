b48493a72d253e9ba2d2f5ff039eedc7f369f22c
Estikno <95084542+Estikno@users.noreply.github.com>
2025-08-14 19:38:44 +0200

    Require padding script

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,3 +1,4 @@
+require("config.autocmds")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -18,7 +19,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- Custom imports
     -- import/override with your plugins
     { import = "plugins" },
   },
